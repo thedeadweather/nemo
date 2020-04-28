c4570b5d5c183383fb8b884dbf4f4c11b8e2f840
Alex Kln <kalininmalinin@gmail.com>
2020-04-29 02:04:54 +0300

    edit design

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,21 @@
 var main = function(){
 
   $('.hidden').hide();
- 
+  
   $('.clickable').click(function(){
     $(this).next().slideToggle({duration: 300});
   });
 
+  $('.appear').click(function(){
+    $(this).next().animate({
+        width: [ "toggle", "swing" ], // ширина элемента
+        height: [ "toggle", "swing" ], // высота элемента
+        opacity: "toggle" // прозрачность элемента
+    }, 1500, "linear"); // продолжительность и скорость анимации
+  });
+
   $('.header-link').click(function(){
     var id = $(this).attr('href');
-    
     $('html, body').animate({
       scrollTop: ($(id).offset().top - 76)
     }, 500);
